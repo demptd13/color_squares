f4ac5713033f4ebcfafe449d8fee72106b940249
dempd13 <demptd13@gmail.com>
2022-06-19 12:44:49 +0200

    box shadow square

--- source/js/main.js
+++ source/js/main.js
@@ -5,7 +5,7 @@ for (i = 0; i < textArray.length; i++) {
   let color = '#' + Math.floor(Math.random() * 16777215).toString(16);
   title[i].textContent += String(textArray[i]);
   title[i].style.color = color;
-  title[i].style.textShadow = "1px 1px 1px" + color;
+  title[i].style.textShadow = "0px 0px 8px" + color;
 }
 
 let allSquares = document.querySelectorAll(".square");
@@ -15,12 +15,14 @@ for (i = 0; i < allSquares.length; i++) {
     let color = '#' + Math.floor(Math.random() * 16777215).toString(16);
     this.style.backgroundColor = color;
     this.style.border = "1px solid" + color;
+    this.style.boxShadow = "0 0 10px" + color;
     this.style.transition = "none";
   }
   allSquares[i].onmouseout = function () {
     this.style.backgroundColor = "transparent";
     this.style.border = "1px solid rgb(54, 54, 54)";
     this.style.transition = "all 3s ease-out";
+    this.style.boxShadow = "none";
   };
 }
 
